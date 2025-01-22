ba1988bf922b93030f425628f53db9699af030dd
Kartik-Bhutra <kartikbhutra02@gmail.com>
2025-01-23 01:44:54 +0530

    initialize socket

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`a user ${socket.id} connected`);
+    socket.on('disconnect', () => console.log(`user ${socket.id} disconnected`));
+});
 
 const port = process.env.PORT;
 server.listen(port, () => console.log(`server is running on port ${port}`));
